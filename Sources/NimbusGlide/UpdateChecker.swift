10114eef9a2ce9adf11fb6789dc3d45b458fe078
Harry <harry@Harrys-MacBook-Air.local>
2026-03-25 21:16:01 +0700

    Release v1.6.7: Update test

--- Sources/NimbusGlide/UpdateChecker.swift
+++ Sources/NimbusGlide/UpdateChecker.swift
@@ -4,7 +4,7 @@ import AppKit
 import Sparkle
 
 class UpdateChecker: ObservableObject {
-    static let currentVersion = "1.6.6"
+    static let currentVersion = "1.6.7"
 
     let updaterController: SPUStandardUpdaterController
 
